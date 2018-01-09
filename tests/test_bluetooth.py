af117e478c9cf23b056a086741c6b3e044d49810
felipessalvatore <felipessalvatore@github.com>
2018-01-09 21:10:37 -0200

    Add test for connection

--- tests/test_bluetooth.py
+++ tests/test_bluetooth.py
@@ -1,17 +1,19 @@
 import unittest
-import bluetooth
+import bluetooth as blue
 
 
 class BluetoothTest(unittest.TestCase):
  
     @classmethod
     def setUp(cls):
-       self.ID = '00:16:53:17:EF:0A' # MAC address NXT11
+       cls.ID = '00:16:53:17:EF:0A' # MAC address NXT11
 
     def test_rasberry_can_find_nxt_via_bluetooth(self):
-        blue_list = bluetooth.discover_devices()
+        blue_list = blue.discover_devices()
         self.assertIn(self.ID, blue_list)
 
     def test_bluetooth_connection(self):
-        blue_list = bluetooth.discover_devices()
-        self.assertIn(self.ID, blue_list)
+        socket = blue.BluetoothSocket(blue.RFCOMM)
+        socket.connect((self.ID, 1))
+        peer_name = socket.getpeername() 
+        self.asserEqual(self.ID, peer_name[0])
