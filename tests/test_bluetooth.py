56ba1817a4b8c0767680126bae2670af222b3761
felipessalvatore <felipessalvatore@github.com>
2018-01-09 21:13:18 -0200

    hotfix

--- tests/test_bluetooth.py
+++ tests/test_bluetooth.py
@@ -3,7 +3,7 @@ import bluetooth as blue
 
 
 class BluetoothTest(unittest.TestCase):
- 
+
     @classmethod
     def setUp(cls):
        cls.ID = '00:16:53:17:EF:0A' # MAC address NXT11
@@ -15,5 +15,5 @@ class BluetoothTest(unittest.TestCase):
     def test_bluetooth_connection(self):
         socket = blue.BluetoothSocket(blue.RFCOMM)
         socket.connect((self.ID, 1))
-        peer_name = socket.getpeername() 
-        self.asserEqual(self.ID, peer_name[0])
+        peer_name = socket.getpeername()
+        self.assertEqual(self.ID, peer_name[0])
