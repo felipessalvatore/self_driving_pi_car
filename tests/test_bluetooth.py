3adb45725294c0c37510f815a968f78ff9d99121
pksm <paulamoraes95@gmail.com>
2018-01-10 16:22:21 -0200

    fix functions name in test_bluetooth and fix test_motor.py

--- tests/test_bluetooth.py
+++ tests/test_bluetooth.py
@@ -8,11 +8,11 @@ class BluetoothTest(unittest.TestCase):
     def setUp(cls):
        cls.ID = '00:16:53:17:EF:0A' # MAC address NXT11
 
-    def test_bluetooth_raspberry_can_find_nxt(self):
+    def test_bluetooth_2_raspberry_can_find_nxt(self):
         blue_list = blue.discover_devices()
         self.assertIn(self.ID, blue_list)
 
-    def test_bluetooth_connection(self):
+    def test_bluetooth_1_connection(self):
         socket = blue.BluetoothSocket(blue.RFCOMM)
         socket.connect((self.ID, 1))
         peer_name = socket.getpeername()
