df954e10f803de21e14f37878f717784e91388e5
felipessalvatore <felipessalvatore@github.com>
2018-01-16 21:50:18 -0200

    Add abstract version of the data collector class

--- self_driving/data_collection/DataCollector.py
+++ self_driving/data_collection/DataCollector.py
@@ -2,100 +2,102 @@ import sys
 import os
 import inspect
 import keyboard as key
-import nxt
 from util import get_date
-import cv2
-import time
+import abc
+
 almost_current = os.path.abspath(inspect.getfile(inspect.currentframe()))
 currentdir = os.path.dirname(almost_current)
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from nxt_car import bluetooth
-from vision.cam import getCamera
+from nxt_car.DiffCar import DiffCar # noqa
+from vision.Camera import Camera # noqa
 
-class DataCollector(object):
+
+class DataCollector(metaclass=abc.ABCMeta): # noqa
     """
     to do
     """
-    def __init__(self, name=""):
-        self.sock, brick = bluetooth.connectCar()
-        self.leftMotor = nxt.Motor(brick, nxt.PORT_C)
-        self.rightMotor = nxt.Motor(brick, nxt.PORT_B)
-        turn_ratio = 0
-        self.both = nxt.SynchronizedMotors(self.leftMotor,
-                                      self.rightMotor,
-                                      turn_ratio)
-        self.dir_name = name + get_date()
+    def __init__(self, robot, cam, name=None):
+        date = get_date()
+        if name is not None:
+            if os.path.exists(name):
+                name += date
+            os.mkdir(name)
+            self.dir_name = os.path.join(name, date)
+            self.pickle_name = os.path.join(name,
+                                            date + '_pickle')
+        else:
+            self.dir_name = date
+            self.pickle_name = os.path.join(date + '_pickle')
         os.mkdir(self.dir_name)
         self.data_dict = {}
         self.count = 0
+        self.robot = robot
+        self.camera = cam
 
-        self.camera = getCamera()
-
-    @staticmethod
-    def saveImage(folder, name, frame):
-        name += ".png"
-        path = os.path.join(folder, name)
-        cv2.imwrite(path, frame)
-
-    def saveDict(self, cmd):
-        self.data_dict[str(self.count)] = cmd
+    def save_image_write_dict(self, image, command):
+        """
+        to do
+        """
+        name = str(self.count) + ".png"
+        name = os.path.join(self.dir_name, name)
+        self.camera.save_image(name, image)
+        self.data_dict[str(self.count)] = command
         self.count += 1
 
-
+    @abc.abstractmethod
     def generate(self):
-	while True:
-            _, im = self.camera.read()
-	    print im.shape
-            if True:
-                if key.is_pressed('q'):
-                    print('Exiting...')
-                    break
+        """
+        to do
+        """
+        return
 
-                elif key.is_pressed('up'):
-                    self.both.run(40)
-                    self.saveImage(self.dir_name, str(self.count),im)
-                    self.saveDict('up')
-                    time.sleep(0.05)
 
-                elif key.is_pressed('down'):
-                    self.both.run(-40)
-                    self.saveImage(self.dir_name, str(self.count),im)
-                    self.saveDict('down')
-                    time.sleep(0.05)
+class BasicDiffCollector(DataCollector):
+    """
+    to do
+    """
 
-                elif key.is_pressed('left'):
-                    self.rightMotor.weak_turn(20,30)
-                    self.leftMotor.weak_turn(-20,30)
-                    self.saveImage(self.dir_name, str(self.count),im)
-                    self.saveDict('left')
+    def __init__(self, robot, cam, name):
+        super().__init__(robot, cam, name)
 
-                elif key.is_pressed('right'):
-                    self.rightMotor.weak_turn(-20,30)
-                    self.leftMotor.weak_turn(20,30)
-                    self.saveImage(self.dir_name, str(self.count),im)
-                    self.saveDict('right')
+    def generate(self, state):
+        while True:
+            img = self.cam.take_picture_rgb()
 
-                # elif key.is_pressed('space'):
-                #     self.leftMotor.idle()
-                #     self.rightMotor.idle()
-                #     self.leftMotor.brake()
-                #     self.rightMotor.brake()
-                #     print('stopping...')
+            print "Working"
 
-                else:
-		    print 'else'
-                    self.leftMotor.idle()
-                    self.rightMotor.idle()
-#            except:
-#		print 'except'
-#                break
+            if key.is_pressed('q'):
+                print('Exiting...')
+                break
+
+            elif key.is_pressed('up'):
+                self.robot.move_up()
+                self.save_image_write_dict(img, 'up')
+                time.sleep(0.05)
+
+            elif key.is_pressed('down'):
+                self.robot.move_down()
+                self.save_image_write_dict(img, 'down')
+                time.sleep(0.05)
+
+            elif key.is_pressed('left'):
+                self.robot.move_left()
+                self.save_image_write_dict(img, 'left')
+
+            elif key.is_pressed('right'):
+                self.robot.move_left()
+                self.save_image_write_dict(img, 'right')
+            else:
+                self.robot.idle()
+
 
 if __name__ == '__main__':
-    dc = DataCollector("teste_1")
-    print("image folder = {}".format(dc.dir_name))
+    name = "basic_test"
+    robot = DiffCar()
+    cam = Camera()
+    dc = BasicDiffCollector(robot, cam, name)
     dc.generate()
-    bluetooth.disconnectCar(dc.sock)
+    bluetooth.disconnectCar(robot.sock)
     print(dc.data_dict)
-
