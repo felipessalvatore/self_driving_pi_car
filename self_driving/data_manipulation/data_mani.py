2021135ae4eed023d8ce7ce8987a2d5c5b71fce5
felipessalvatore <felipessalvatore@github.com>
2018-01-22 11:31:04 -0200

    Fix pickle problem, add argparse

--- self_driving/data_manipulation/data_mani.py
+++ self_driving/data_manipulation/data_mani.py
@@ -1,34 +1,23 @@
-# import argparse
-# import inspect
-# import pickle
+import argparse
+import pickle
 import os
 from scipy import misc
-import cPickle as pickle
 import numpy as np
 
-command_dict = {"up": 0, "down": 1, "left": 2, "right": 3}
-
-
-def command2int(command):
-    """
-    Command to int representation
-
-    :param command: command label
-    :type command: string
-    :rtype: int
-    """
-    return command_dict[command]
+command2int = {"up": 0, "down": 1, "left": 2, "right": 3}
+int2command = {i[1]: i[0] for i in command2int.items()}
 
 
 def folder2array(folder_path,
                  pickle_path,
-                 image_width=160,
-                 image_height=90,
-                 image_channels=3):
+                 image_width,
+                 image_height,
+                 image_channels):
     """
     Function to transform all images from the folder folder_name
     into a tuple of np arrays.
 
+    :param
     :type folder_path: str
     :type
     :rtype: (np.array,np.array
@@ -44,39 +33,46 @@ def folder2array(folder_path,
     print("Trying to convert images from {} \n".format(folder_path))
     for filename in os.listdir(folder_path):
         key = filename[:- 4]
-        label = command2int(label_dict[key])
+        label = command2int[label_dict[key]]
         image_path = os.path.join(folder_path, filename)
-        image = feature_normalization(misc.imread(image_path))
+        image = change_type_to_uint8(misc.imread(image_path))
         image = image.reshape(flat_shape)
         all_images.append(image)
         all_labels.append(label)
-    all_labels = np.array(all_labels)
+    all_labels = change_type_to_uint8(np.array(all_labels))
     all_labels = all_labels.reshape((all_labels.shape[0], 1))
     all_images = np.array(all_images)
     return all_images, all_labels
 
 
-def feature_normalization(image):
+def change_type_to_uint8(image):
     """
-    Feature normalization for otimization
-    https://en.wikipedia.org/wiki/Feature_scaling
+    Change type to uint8 Unsigned integer (0 to 255)
 
     :type image: np array
+    :param
     :rtype: np array
     """
-    image = image.astype('float32')
-    return image / 255
+    image = image.astype('uint8')
+    return image
 
 
-def create_data_set_as_pickle(folder_path, pickle_name="data"):
+def create_data_set_as_np_array(folder_path,
+                                data_name="data",
+                                label_name="labels",
+                                image_width=160,
+                                image_height=90,
+                                image_channels=3):
     """
     Giving one path to a folder of folders of images,
-    this function do all the preprocessing
-    and stores the data as a pickle.
+    this function transform all images in two arrays
+    one 'data_name' with all the flatted images
+    and other 'label_name' with all the respective labels
 
     :type folder_name: str
-    :type pickle_name: str
-    :type verbose: boolean
+    :param
+    :type data_name: str
+    :type data_name: str
     """
     assert os.path.exists(folder_path)
     all_images = []
@@ -86,23 +82,55 @@ def create_data_set_as_pickle(folder_path, pickle_name="data"):
         if os.path.isdir(folder):
             print(folder)
             pickle_path = folder + "_pickle"
-            images, labels = folder2array(folder, pickle_path)
+            images, labels = folder2array(folder,
+                                          pickle_path,
+                                          image_width,
+                                          image_height,
+                                          image_channels)
             all_images.append(images)
             all_labels.append(labels)
     all_images = np.concatenate(all_images, axis=0)
     all_labels = np.concatenate(all_labels, axis=0)
     print(all_images.shape, all_labels.shape)
-    pickle_name += ".p"
-    dict_file = {'images': all_images,
-                 'labels': all_labels}
-    # p = open(pickle_name, "wb")
-    # pickle.dump(dict_file, p, protocol=pickle.HIGHEST_PROTOCOL)
-    # p.close()
-    with open(pickle_name, 'wb') as f:
-        pickle.dump(dict_file, f)
-    print("\npickle can be found in {}".format(pickle_name))
+    np.save(data_name, all_images)
+    np.save(label_name, all_labels)
+
+
+def main():
+    """
+    to do
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('img_folder_path',
+                        type=str, help='path to image folder')
+    parser.add_argument('data_path',
+                        type=str, help='path to data to be saved')
+    parser.add_argument('labels_path',
+                        type=str, help='path to labels to be saved')
+    parser.add_argument("-w",
+                        "--image_width",
+                        type=int,
+                        default=160,
+                        help="width number (default=160)")
+    parser.add_argument("-H",
+                        "--image_height",
+                        type=int,
+                        default=90,
+                        help="height number (default=90)")
+    parser.add_argument("-c",
+                        "--image_channels",
+                        type=int,
+                        default=3,
+                        help="number of channels (default=3)")
+    user_args = parser.parse_args()
+    create_data_set_as_np_array(user_args.img_folder_path,
+                                user_args.data_path,
+                                user_args.labels_path,
+                                user_args.image_width,
+                                user_args.image_height,
+                                user_args.image_channels)
 
 
 if __name__ == '__main__':
-    folder_path = "/home/felsal/Desktop/pista1"
-    create_data_set_as_pickle(folder_path)
+    main()
