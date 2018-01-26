b838ca6d2b94a8d919fd37053c97f71934453192
pksm <paulaksm@ime.usp.br>
2018-01-26 12:53:26 -0200

    add data augmentation, image_manipulation and script to check npy file content

--- self_driving/data_manipulation/data_aug.py
+++ self_driving/data_manipulation/data_aug.py
@@ -1,35 +1,113 @@
+import os
 import argparse
 import numpy as np
-from PIL import Image, ImageDraw
+import image_manipulation
+# from PIL import Image
 
 command2int = {"up": 0, "down": 1, "left": 2, "right": 3}
 int2command = {i[1]:i[0] for i in command2int.items()}
 
-data = np.load("data.npy")
-labels = np.load("labels.npy")
 
-print("data shape = {}".format(data.shape))
-print("data type = {}".format(data.dtype))
-print("labels shape = {}".format(labels.shape))
-print("labels type = {}".format(labels.dtype))
+def get_image_and_command(data_index, 
+                         label_index):
+    """
+    Get and reshape image with parameters: 90(height), 160(width), 3(channels) from data_index
+    and get it's label in label_index (e.g. 'right') 
 
-def get_image_array_and_command(index):
-    img_array = data[index].reshape((90, 160,3))
-    command = int2command[labels[index][0]]
+    :type data_index: numpy.ndarray
+    :type label_index: numpy.ndarray
+    :rtype: numpy.ndarray, str
+    """
+    img_array = data_index.reshape((90, 160,3))
+    command = int2command[label_index[0]]
     return img_array, command
 
-def flip_horizontal_axis(data_name, label_name):
+def get_image(data_index):
+    """
+    Get and reshape image with parameters: 90(height), 160(width), 3(channels) from data_index
+
+    :type data_index: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    return data_index.reshape((90, 160,3))
+
+def get_flat_shape(image):
+    """
+    Multiply each shape component of image (tuple of array dimensions)
+
+    :type image: numpy.ndarray
+    :rtype: int
+    """
+    flat = 1
+    for i in range(len(image.shape)):
+        flat *= image.shape[i] 
+    return flat
+
+def shape2filename(data_shape):
+    """
+    Get each shape component and return a string formatted to 'height_width_channels_'
+
+    :type data_shape: numpy.ndarray
+    :rtype: str
+    """
+    name = ""
+    for i in data_shape:
+        name += "{}_".format(i)
+    return name
+
+def load_dataset(data_path, 
+                 labels_path):
+    """
+    Load and return dataset arrays from data_path and label arrays from labels_path
+
+    :type data_path: str (.npy file)
+    :type labels_path: str (.npy file)
+    :rtype: numpy.ndarray, numpy.ndarray
+    """
+    data = np.load(data_path)
+    labels = np.load(labels_path)
+    return data, labels
+
+def save_dataset(data, 
+                 labels, 
+                 folder_path, 
+                 data_shape, 
+                 name):
+    """
+    Save data and labels in a directory as a numpy array binary file (NPY)
+
+    :type data: numpy.ndarray
+    :type labels: numpy.ndarray
+    :type folder_path: str 
+    :type data_shape: tuple of numpy array dimensions 
+    :type name: str 
+    :rtype: 
+    """
+    shape = shape2filename(data_shape)
+    data_name = "{}_{}data.npy".format(name, shape)
+    label_name = "{}_{}labels.npy".format(name, shape)
+    data_path = os.path.join(folder_path,data_name)
+    labels_path = os.path.join(folder_path,label_name)
+    np.save(data_path, data)
+    np.save(labels_path, labels)
+
+def extend_dataset_flip_axis(data, 
+                             labels):
+    """
+    Balance and extend dataset by generating new images flipping the horizontal axis (only applicable to images labeled 'left' or 'right')
+
+    :type data: numpy.ndarray
+    :type labels: numpy.ndarray
+    :rtype: numpy.ndarray, numpy.ndarray
+    """
     all_images = []
     all_labels = []
-    image_width = 160
-    image_height = 90
-    image_channels = 3
-    flat_shape = image_width * image_height * image_channels
+    flat_shape = data.shape[1]
     for i in range(data.shape[0]):
         orig_label = labels[i]
         if (orig_label / 2 < 1):
             continue
-        frame, cmd = get_image_array_and_command(i)
+        frame, cmd = get_image_and_command(data[i], labels[i])
         if (orig_label % 2 == 0):
             flip_cmd = 3
         else:
@@ -37,37 +115,58 @@ def flip_horizontal_axis(data_name, label_name):
         flip = np.flip(frame, axis=1)
         flip = np.array(flip.reshape(flat_shape))
         all_images.append(flip)
-        all_labels.append(flip_cmd)
-
-
+        all_labels.append(flip_cmd)       
     all_labels = np.array(all_labels).astype('uint8')
     all_labels = all_labels.reshape((all_labels.shape[0], 1))
-   
-    ext_images = np.concatenate((data, all_images), axis=0)
-    ext_labels = np.concatenate((labels, all_labels), axis=0)
-    print(ext_images.shape, ext_labels.shape)
-    np.save(data_name, ext_images)
-    np.save(label_name, ext_labels)
+    extended_images = np.concatenate((data, all_images), axis=0)
+    extended_labels = np.concatenate((labels, all_labels), axis=0)
+    return extended_images, extended_labels
 
+def dataset_augmentation(data):
+    """
+    Create new datasets by applying in data image transformations, available at image_manipulation. 
+    Returns a new dataset and the original shape of its contents
 
-def binarize_dataset():
-    # to do
-    return 
+    :type data: numpy.ndarray
+    :rtype: numpy.ndarray, tuple
+    """
+    new_dataset = []
+    for i in range(data.shape[0]):
+        image = get_image(data[i])
+        new_image = image_manipulation.binarize_image(image)
 
-def green_dataset():
-    # to do
-    return
+        original_shape = new_image.shape
+        new_image = new_image.reshape(get_flat_shape(new_image))
+        new_dataset.append(new_image)
+    new_dataset = np.array(new_dataset).astype('uint8')
+    return new_dataset, original_shape
+
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('data_path',
-                        type=str, help='path to data to be saved')
+                        type=str, help='path to current data')
     parser.add_argument('labels_path',
-                        type=str, help='path to labels to be saved')
+                        type=str, help='path to current labels')
+    parser.add_argument('new_data_folder_path',
+                        type=str, help='path to data and labels to be saved')    
+    parser.add_argument('dataset_name',
+                        nargs='?', default='dataset', type=str, help='name for dataset. (Default) dataset')
+
     user_args = parser.parse_args()
-    flip_horizontal_axis(user_args.data_path,
-                         user_args.labels_path)
+
+    data, labels = load_dataset(user_args.data_path, 
+                                user_args.labels_path)
+    data, labels = extend_dataset_flip_axis(data, 
+                                            labels)
+    new_dataset, data_shape = dataset_augmentation(data)
+    save_dataset(new_dataset, 
+                 labels, 
+                 user_args.new_data_folder_path,
+                 data_shape,
+                 user_args.dataset_name)
+
 
 if __name__ == '__main__':
     main()
---
+++ self_driving/data_manipulation/image_manipulation.py
@@ -0,0 +1,82 @@
+'''      
+Useful functions for data augmentation of images  
+'''
+import cv2
+import numpy as np
+from scipy import ndimage
+
+def grayscale_image(input_image):
+    """
+    Convert input_image to grayscale 
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    return cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+def binarize_image(input_image):
+    """
+    Convert input_image to binary representation
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    gray_image = grayscale_image(input_image)
+    bin_image = cv2.GaussianBlur(gray_image, (5,5),0)
+    _, bin_image = cv2.threshold(bin_image, 177 ,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return bin_image
+
+def green_channel(input_image):  
+    """
+    Split input_image channels and return only the green channel 
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    return input_image[:,:,1]
+
+def top_bottom_cut(input_image): 
+    """
+    Cut off randomly part of the top and bottom of input_image and reshape it to the original dimensions
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    height = input_image.shape[0]
+    width = input_image.shape[1]
+    input_dtype = input_image.dtype
+    top = int(np.random.uniform(.325, .425) * height)
+    bottom = int(np.random.uniform(.075, .175) * height)
+    input_image = input_image[top:-bottom, :]
+    img = Image.fromarray(input_image)
+    img = img.resize((width,height), Image.LANCZOS)
+    cut_image = np.array(img).astype(input_dtype)
+    return cut_image
+
+def random_shadow(input_image):
+    """
+    Insert a vertical random shadow in an input_image  
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    height, width = input_image.shape[0], input_image.shape[1]
+    [x1, x2] = np.random.choice(width, size=2, replace=False)
+    k = height / float(x2 - x1)
+    b = - k * x1
+    im_array = input_image.copy()
+    for i in range(height):
+        c = int((i - b) / k)
+        im_array[i, :c, :] = (im_array[i, :c, :] * .5).astype(np.uint8)
+    return im_array
+
+def gaussian_blur(input_image,
+                  kernel_size=5):
+    """
+    Blur input_image with a Gaussian convolution  
+
+    :type input_image: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    blur = ndimage.filters.gaussian_filter(input_image, sigma=(kernel_size,kernel_size,1))
+    return blur
