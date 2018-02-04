1220167e286d5a70a738d507efed016703cc8993
felipessalvatore <felipessalvador@googlemail.com>
2018-02-04 13:38:11 -0200

    move all tf functions to util

--- self_driving/ml_training/util.py
+++ self_driving/ml_training/util.py
@@ -21,6 +21,16 @@ def run_test(testClass):
 
 
 def reconstruct_from_record(record_path, bound=1000):
+    """
+    Function to transform a tf records into a tuple of
+    np arrays. The size is controled by the param "bound".
+
+    :param record_path: path to tf_record
+    :type record_path: str
+    :param bound: number of examples to be read
+    :type bound: int
+    :rtype: np.array, np.array
+    """
     reconstructed_images = []
     reconstructed_labels = []
     record_iterator = tf.python_io.tf_record_iterator(path=record_path)
@@ -29,9 +39,9 @@ def reconstruct_from_record(record_path, bound=1000):
         if i <= bound:
             example = tf.train.Example()
             example.ParseFromString(string_record)
-            height = int(example.features.feature['height'].int64_list.value[0]) # noqa
-            width = int(example.features.feature['width'].int64_list.value[0]) # noqa
-            channels = int(example.features.feature['channels'].int64_list.value[0]) # noqa
+            height = int(example.features.feature['height'].int64_list.value[0])  # noqa
+            width = int(example.features.feature['width'].int64_list.value[0])  # noqa
+            channels = int(example.features.feature['channels'].int64_list.value[0])  # noqa
             img_string = (example.features.feature['image_raw']
                                           .bytes_list
                                           .value[0])
@@ -53,11 +63,19 @@ def reconstruct_from_record(record_path, bound=1000):
 
 
 def accuracy_per_category(pred, label, categories):
+    """
+    Function to give the model's accuracy for each category.
+
+    :param pred: model's prediction
+    :type pred: np.array
+    :param label: true labels
+    :type label: np.array
+    :rtype: list of float
+    """
     pred, label = list(pred), list(label)
     results = []
     for cat in range(categories):
-        f = lambda x: 1 if x == cat else 0 # noqa
-        vfunc = np.vectorize(f)
+        vfunc = np.vectorize(lambda x: 1 if x == cat else 0)
         mapped_pred = vfunc(pred)
         mapped_labels = vfunc(label)
         right = float(np.dot(mapped_pred, mapped_labels))
@@ -114,3 +132,58 @@ def get_random_architecture_and_activations(network_sizes,
         hidden.append(categories)
 
     return hidden_layers, activations
+
+
+def parser_with_normalization(tfrecord):
+    """
+    Parser function, transforming string into
+    a tuple of tensors.
+
+    :param tfrecord: a single binary serialized
+    :type tfrecord: tf.Tensor(shape=(), dype=tf.string)
+    :rtype: tf.Tensor(shape=(1, height*width*channels),
+                      dtype=tf.float32),
+            tf.Tensor(shape=(1,), dtyṕe=tf.int32)
+    """
+    features = {'height': tf.FixedLenFeature([], tf.int64),
+                'width': tf.FixedLenFeature([], tf.int64),
+                'channels': tf.FixedLenFeature([], tf.int64),
+                'image_raw': tf.FixedLenFeature([], tf.string),
+                'labels_raw': tf.FixedLenFeature([], tf.string)}
+
+    tfrecord_parsed = tf.parse_single_sequence_example(
+        tfrecord, features)
+
+    image = tf.decode_raw(tfrecord_parsed[0]['image_raw'], tf.uint8)
+    image = tf.cast(image, tf.float32) / 255
+
+    label = tf.decode_raw(tfrecord_parsed[0]['labels_raw'], tf.uint8)
+    label = tf.cast(label, tf.int32)
+
+    return image, label
+
+
+def get_iterator(filename, batch_size, parser):
+    """
+    Function to get an interator.
+
+    :param filename: path to tfrecord dataset
+    :type filename: str
+    :param batch_size: size of the batch
+    :type batch_size: int
+    :param parser: function to parse a string
+                   into a tensor
+    :type parser: tf.Tensor(shape=(), dype=tf.string)
+                  ->
+                  tf.Tensor(shape=(1, height*width*channels),
+                      dtype=tf.float32),
+                  tf.Tensor(shape=(1,), dtyṕe=tf.int32)
+    :rtype: tf.contrib.data.Iterator
+    """
+    dataset = tf.contrib.data.TFRecordDataset(filename)
+    dataset = dataset.map(parser)
+    dataset = dataset.repeat()
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.shuffle(batch_size * 2)
+    iterator = dataset.make_initializable_iterator()
+    return iterator
