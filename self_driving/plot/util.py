129095d1ae314c16e870cfbc0d22ff3c3a7f835f
felipessalvatore <felipessalvatore@github.com>
2018-02-05 11:58:25 -0200

    Remove path as argument, hotfix pip8 warnings

--- self_driving/plot/util.py
+++ self_driving/plot/util.py
@@ -21,31 +21,6 @@ def plotconfusion(truth, predictions):
     cm = ConfusionMatrix(truth, predictions)
     plt.figure(figsize=(9, 9))
     cm.plot(backend='seaborn')
-    title = "Confusion matrix of {} examples\n accuracy = {}".format(size, acc) 
+    title = "Confusion matrix of {} examples\n accuracy = {}".format(size, acc)
     plt.title(title, fontsize=24, fontweight='bold')
     plt.show()
-
-
-def accuracy_per_category(pred, label, categories, cat_dict=None):
-    pred, label = list(pred), list(label)
-    results = []
-    result_str = ''
-    for cat in range(categories):
-        f = lambda x: 1 if x == cat else 0 # noqa
-        vfunc = np.vectorize(f)
-        mapped_pred = vfunc(pred)
-        mapped_labels = vfunc(label)
-        right = float(np.dot(mapped_pred, mapped_labels))
-        total = np.sum(mapped_labels)
-        if total == 0:
-            results.append(0.0)
-        else:
-            results.append((right / total))
-
-    for i, result in enumerate(results):
-        if cat_dict is None:
-            result_str += str(i) + " : {0:.3f}\n".format(result)
-        else:
-            result_str += cat_dict[i] + " : {0:.3f}\n".format(result)
-
-    return result_str
