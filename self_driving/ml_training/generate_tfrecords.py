13c5f4a5699a31961b1948c876fd6f711309997e
felipessalvatore <felipessalvatore@github.com>
2018-02-06 14:44:03 -0200

    add docstring

--- self_driving/ml_training/generate_tfrecords.py
+++ self_driving/ml_training/generate_tfrecords.py
@@ -1,19 +1,43 @@
-import tensorflow as tf
-import os
 import argparse
 from Config import Config
 from DataHolder import DataHolder
-from util import int2command
 
-def data_search(channels, 
-                data_path, 
-                label_path, 
-                name,
-                flip=False,
-                augmentation=False,
-                gray=False,
-                green=False,
-                binary=False):
+
+def records_generator(channels,
+                      data_path,
+                      label_path,
+                      name,
+                      flip=False,
+                      augmentation=False,
+                      gray=False,
+                      green=False,
+                      binary=False):
+    """
+    Generates tfrecords.
+
+    :param config: config class with all hyper param information
+    :type config: Config
+    :param data_path: path to load data np.array
+    :type data_path: str
+    :param record_path: path to load labels np.array
+    :type label_path: str
+    :param name: path to save tfrecord
+    :type name: str
+    :param flip: param to control if the data
+                         will be flipped
+    :type flip: boolean
+    :param augmentation: param to control if the data
+                         will augmented
+    :type augmentation: boolean
+    :param gray: param to control if the data
+                 will be grayscale images
+    :type gray: boolean
+    :param green: param to control if the data will use only
+                  the green channel
+    :type green: boolean
+    :param binary: param to control if the data will be binarized
+    :type binary: boolean
+    """
 
     config = Config(channels=channels)
     data = DataHolder(config,
@@ -26,8 +50,8 @@ def data_search(channels,
                       green=green,
                       binary=binary,
                       records=None)
+    data.create_records()
 
-    data.create_records() 
 
 def main():
     """
@@ -39,24 +63,24 @@ def main():
                         type=str,
                         default="data",
                         help="name for tfrecords e.g. pure, flip, aug, bin, gray, green (default=data)")  # noqa
-    
+
     parser.add_argument("-f",
                         "--flip",
                         action="store_true",
                         default=False,
-                        help="flag to flip x-axis (default=False)")  # noqa
-    
+                        help="flag to flip x-axis (default=False)")
+
     parser.add_argument("-p",
                         "--pure",
                         action="store_true",
                         default=False,
-                        help="flag to pure (default=False)")  # noqa
+                        help="flag to pure (default=False)")
 
     parser.add_argument("-a",
                         "--augmentation",
                         action="store_true",
                         default=False,
-                        help="flag to augment dataset (default=False)")  # noqa
+                        help="flag to augment dataset (default=False)")
 
     parser.add_argument("-gy",
                         "--gray",
@@ -74,14 +98,14 @@ def main():
                         "--binary",
                         action="store_true",
                         default=False,
-                        help="flag to binarize dataset (default=False)")  # noqa
- 
+                        help="flag to binarize dataset (default=False)")
+
     parser.add_argument("-d",
                         "--train_data",
                         type=str,
                         default=None,
                         help="train data path (default=None)")
- 
+
     parser.add_argument("-l",
                         "--train_label",
                         type=str,
@@ -89,28 +113,20 @@ def main():
                         help="label data path (default=None)")
 
     args = parser.parse_args()
-    
-    #if args.mode == "bin" or args.mode == "gray" or args.mode == "green":
-    #    channels = 1
-    #else:
-    #    channels = 3
     if args.binary or args.green or args.gray:
         channels = 1
     else:
-        channels = 3 
-    #cond1 = type(args.train_data) == str
-    #cond2 = type(args.train_label) == str
-    #have_records = not (cond1 and cond2)
-    data_search(channels, 
-                args.train_data,
-                args.train_label, 
-                args.name,
-                args.flip,
-                args.augmentation,
-                args.gray,
-                args.green,
-                args.binary)
+        channels = 3
+    records_generator(channels,
+                      args.train_data,
+                      args.train_label,
+                      args.name,
+                      args.flip,
+                      args.augmentation,
+                      args.gray,
+                      args.green,
+                      args.binary)
+
 
 if __name__ == "__main__":
     main()
-
