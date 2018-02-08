1061a6007b0cf92285324b5fb91d10755fb9df6f
felipessalvatore <felipessalvatore@github.com>
2018-02-08 18:57:41 -0200

    Add a better accuracy script

--- self_driving/ml_training/acc_test.py
+++ self_driving/ml_training/acc_test.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import os
 import numpy as np
-import shutil
 import argparse
 import sys
 import inspect
@@ -22,24 +21,17 @@ sys.path.insert(0, parentdir)
 from plot.util import plotconfusion # noqa
 
 
-def train(mode,
-          records,
-          height,
-          width,
-          channels,
-          architecture,
-          activations,
-          batch_size,
-          epochs,
-          num_steps,
-          save_step,
-          learning_rate,
-          optimizer,
-          verbose,
-          name,
-          move):
+def acc(mode,
+        records,
+        height,
+        width,
+        channels,
+        architecture,
+        activations,
+        test,
+        name):
     """
-    Trains a model
+    Checks model's accuracy
 
     :param height: image height
     :type heights: int
@@ -51,51 +43,17 @@ def train(mode,
     :type architecture: list of int
     :param activations: list of different tf functions
     :type activations: list of tf.nn.sigmoid, tf.nn.relu, tf.nn.tanh
-    :param batch_size: batch size for training
-    :type batch_size: int
-    :param epochs: number of epochs
-    :type epochs: int
-    :param num_steps: number of iterations for each epoch
-    :type num_steps: int
-    :param save_step: when step % save_step == 0, the model
-                      parameters are saved.
-    :type save_step: int
-    :param learning_rate: learning rate for the optimizer
-    :type learning_rate: float
-    :param optimizer: a optimizer from tensorflow.
-    :type optimizer: tf.train.GradientDescentOptimizer,
-                     tf.train.AdadeltaOptimizer,
-                     tf.train.AdagradOptimizer,
-                     tf.train.AdagradDAOptimizer,
-                     tf.train.AdamOptimizer,
-                     tf.train.FtrlOptimizer,
-                     tf.train.ProximalGradientDescentOptimizer,
-                     tf.train.ProximalAdagradOptimizer,
-                     tf.train.RMSPropOptimizer
-    :param verbose: param to control if the trainig will be printed
-                    and if the confusion matrix will be calculated.
-    :type verbose: bool
+    :param test: param to control if the test accuracy will be printed.
+    :type test: bool
     :param name: name to save the confusion matrix plot.
     :type name: str
-    :param move: param to control if the checkpoints path
-                 will be moved to the parent folder.
-    :type move: bool
     """
-
-    # if os.path.exists("checkpoints"):
-    #     shutil.rmtree("checkpoints")
 
     config = Config(height=height,
                     width=width,
                     channels=channels,
                     architecture=architecture,
-                    activations=activations,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    num_steps=num_steps,
-                    save_step=save_step,
-                    learning_rate=learning_rate,
-                    optimizer=optimizer)
+                    activations=activations)
 
     data = DataHolder(config,
                       records=records)
@@ -103,23 +61,28 @@ def train(mode,
     graph = tf.Graph()
     network = DFN(graph, config)
     trainer = Trainer(graph, config, network, data)
-    print("\nTraining in the {} data\n".format(mode))
+    print("\nAccuracy in the {} data\n".format(mode))
     print("params:\n{}\n".format(config.get_status()))
-    # trainer.fit(verbose=verbose)
-    if verbose:
-        valid_images, valid_labels, _ = reconstruct_from_record(data.get_test_tfrecord()) # noqa
-        valid_images = valid_images.astype(np.float32) / 255
-        valid_pred = trainer.predict(valid_images)
-        valid_labels = valid_labels.reshape((valid_labels.shape[0],))
-        plotconfusion(valid_labels, valid_pred, name + ".png", int2command, classes=["left", "right", "up"], bound=10000) # noqa
-    if move:
-        dst = os.path.join(parentdir, "checkpoints")
-        shutil.move("checkpoints", dst)
+    if not os.path.exists("checkpoints"):
+        print("===Accuracy of a non trained model===")
+
+    valid_images, valid_labels, _ = reconstruct_from_record(data.get_valid_tfrecord(), bound=10000) # noqa
+    valid_images = valid_images.astype(np.float32) / 255
+    valid_pred = trainer.predict(valid_images)
+    valid_labels = valid_labels.reshape((valid_labels.shape[0],))
+    plotconfusion(valid_labels, valid_pred, name + "_valid.png", int2command, classes=["left", "right", "up"]) # noqa
+
+    if test:
+        test_images, test_labels, _ = reconstruct_from_record(data.get_test_tfrecord(), bound=10000) # noqa
+        test_images = test_images.astype(np.float32) / 255
+        test_pred = trainer.predict(test_images)
+        test_labels = test_labels.reshape((test_labels.shape[0],))
+        plotconfusion(test_labels, test_pred, name + "_test.png", int2command, classes=["left", "right", "up"]) # noqa
 
 
 def main():
     """
-    Main script to train one model using one kind of data.
+    Main script to check model's accuracy using one kind of data.
 
     "mode" is the argument to choose which kind of data will be used:
         "pure": rgb image with no manipulation.
@@ -159,59 +122,16 @@ def main():
                         nargs='+',
                         help='activations: relu, sigmoid, tanh (defaul=None)',
                         default=None)
-    parser.add_argument("-b",
-                        "--batch_size",
-                        type=int,
-                        default=32,
-                        help="batch size (default=32)")
-    parser.add_argument("-e",
-                        "--epochs",
-                        type=int,
-                        default=5,
-                        help="epochs for training (default=5)")
-    parser.add_argument("-ns",
-                        "--num_steps",
-                        type=int,
-                        default=1000,
-                        help="number of steps for each epoch (default=1000)")
-    parser.add_argument("-ss",
-                        "--save_step",
-                        type=int,
-                        default=100,
-                        help="number of steps to save variables (default=100)")
-    parser.add_argument("-lr",
-                        "--learning_rate",
-                        type=float,
-                        default=0.02,
-                        help="learning rate (default=0.02)")
-    opt_list = """optimizers: GradientDescent,
-                              Adadelta,
-                              Adagrad,
-                              Adam,
-                              Ftrl,
-                              ProximalGradientDescent,
-                              ProximalAdagrad,
-                              RMSProp"""
-    parser.add_argument("-o",
-                        "--optimizer",
-                        type=str,
-                        default="GradientDescent",
-                        help=opt_list + "(default=GradientDescent)")
-    parser.add_argument("-v",
-                        "--verbose",
+    parser.add_argument("-t",
+                        "--test",
                         action="store_true",
                         default=False,
-                        help="print training results and calculate confusion matrix (default=False)")  # noqa
+                        help="print test results and calculate confusion matrix (default=False)")  # noqa
     parser.add_argument("-n",
                         "--name",
                         type=str,
                         default="Confusion_Matrix",
                         help="name to save confusion matrix plot (default=Confusion_Matrix)")  # noqa
-    parser.add_argument("-mv",
-                        "--move",
-                        action="store_true",
-                        default=False,
-                        help="move checpoits to parent folder (default=False)")  # noqa
     args = parser.parse_args()
     if args.mode == "bin" or args.mode == "gray" or args.mode == "green":
         channels = 1
@@ -223,15 +143,6 @@ def main():
         record = args.mode + record
         new_records.append(record)
 
-    optimizer_dict = {"GradientDescent": tf.train.GradientDescentOptimizer, # noqa
-                      "Adadelta": tf.train.AdadeltaOptimizer,
-                      "Adagrad": tf.train.AdagradOptimizer,
-                      "Adam": tf.train.AdamOptimizer,
-                      "Ftrl": tf.train.FtrlOptimizer,
-                      "ProximalGradientDescent": tf.train.ProximalGradientDescentOptimizer, # noqa
-                      "ProximalAdagrad": tf.train.ProximalAdagradOptimizer, # noqa
-                      "RMSProp":tf.train.RMSPropOptimizer} # noqa
-
     activations_dict = {"relu": tf.nn.relu,
                         "sigmoid": tf.nn.sigmoid,
                         "tanh": tf.nn.tanh}
@@ -239,24 +150,16 @@ def main():
         activations = [activations_dict[act] for act in args.activations]
     else:
         activations = args.activations
-    optimizer = optimizer_dict[args.optimizer]
 
-    train(mode=args.mode,
-          records=new_records,
-          height=args.height,
-          width=args.width,
-          channels=channels,
-          architecture=args.architecture,
-          activations=activations,
-          batch_size=args.batch_size,
-          epochs=args.epochs,
-          num_steps=args.num_steps,
-          save_step=args.save_step,
-          learning_rate=args.learning_rate,
-          optimizer=optimizer,
-          verbose=args.verbose,
-          name=args.name,
-          move=args.move)
+    acc(mode=args.mode,
+        records=new_records,
+        height=args.height,
+        width=args.width,
+        channels=channels,
+        architecture=args.architecture,
+        activations=activations,
+        test=args.test,
+        name=args.name)
 
 
 if __name__ == '__main__':
