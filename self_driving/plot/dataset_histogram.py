44fd6bc1094dea15d733a5a64929bbc0caff69c2
felipessalvatore <felipessalvatore@github.com>
2018-02-10 22:46:13 -0200

    update histogram to save png

--- self_driving/plot/dataset_histogram.py
+++ self_driving/plot/dataset_histogram.py
@@ -6,38 +6,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_histogram(labels):
+def plot_histogram(labels, path):
     """
     Plot dataset histogram
 
     :param label_path: array of labels
     :type label_path: np.array
+    :param path: name to save histogram
+    :type path: np.str
     """
 
-    data_hist = plt.hist(labels, bins=np.arange(5) - 0.5, edgecolor='black')
+    data_hist = plt.hist(labels, bins=np.arange(4) - 0.5, edgecolor='black')
     axes = plt.gca()  # Get Current Axes
     axes.set_ylim([0, len(labels)])
 
     plt.title("Label histogram of {} image dataset".format(len(labels)))
-    plt.xticks(np.arange(5), ['up', 'down', 'left', 'right'])
+    plt.xticks(np.arange(4), ['up', 'left', 'right'])
     plt.xlabel("Label")
     plt.ylabel("Frequency")
 
-    for i in range(4):
+    for i in range(3):
         plt.text(data_hist[1][i] + 0.25,
                  data_hist[0][i] + (data_hist[0][i] * 0.01),
                  str(int(data_hist[0][i])))
 
-    plt.show()
+    plt.savefig(path)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('labels_path',
                         type=str, help='path to labels')
-    user_args = parser.parse_args()
-    labels = np.load(user_args.labels_path)
-    plot_histogram(labels)
+    parser.add_argument("-n",
+                        "--name",
+                        type=str,
+                        default="histogram.png",
+                        help="name to save histogram plot (default=histogram.png)")  # noqa
+    args = parser.parse_args()
+    labels = np.load(args.labels_path)
+    plot_histogram(labels, args.name)
 
 
 if __name__ == '__main__':
