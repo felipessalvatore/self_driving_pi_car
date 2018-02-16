7fbc36bae427ea42a73b315c79be9d90cf597dcb
felipessalvatore <felipessalvatore@github.com>
2018-02-16 14:43:07 -0200

    add CNN and complete argparse

--- self_driving/ml_training/best_learning_rate.py
+++ self_driving/ml_training/best_learning_rate.py
@@ -11,13 +11,29 @@ from DataHolder import DataHolder
 from Config import Config
 from Trainer import Trainer
 from DFN import DFN
+from CNN import CNN
 from util import reconstruct_from_record, accuracy_per_category
 from util import int2command
 
 
-def lr_search(experiments,
+def lr_search(mode,
+              records,
+              height,
+              width,
               channels,
-              records=None):
+              architecture,
+              activations,
+              conv_architecture,
+              kernel_sizes,
+              pool_kernel,
+              batch_size,
+              epochs,
+              num_steps,
+              save_step,
+              optimizer,
+              experiments,
+              conv,
+              divisor):
     """
     Script to run different experiments
     to search a learning rate value,
@@ -30,25 +46,45 @@ def lr_search(experiments,
     :param records: list of paths to tf_records
     :type records: none or list of str
     """
-    LR = np.random.random_sample([experiments]) / 100
-    LR = np.append(LR, 0.02)
-    experiments += 1
+    LR = np.random.random_sample([experiments]) / divisor
     LR.sort()
     numeric_result = []
     results = []
     info = []
     LR = list(LR)
+    if conv:
+        net_name = "CNN"
+    else:
+        net_name = "DFN"
 
+    header = "\nSearching {} learing rate in the {} data\n".format(net_name,
+                                                                   mode)
+    print(header)
     for lr in LR:
-        config = Config(channels=channels,
-                        learning_rate=lr)
+        config = Config(height=height,
+                        width=width,
+                        channels=channels,
+                        learning_rate=lr,
+                        architecture=architecture,
+                        activations=activations,
+                        conv_architecture=conv_architecture,
+                        kernel_sizes=kernel_sizes,
+                        pool_kernel=pool_kernel,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        num_steps=num_steps,
+                        save_step=save_step,
+                        optimizer=optimizer)
 
         data = DataHolder(config,
                           records=records)
         name = "lr = {0:.6f}".format(lr)
         print(name + ":\n")
         graph = tf.Graph()
-        network = DFN(graph, config)
+        if conv:
+            network = CNN(graph, config)
+        else:
+            network = DFN(graph, config)
         trainer = Trainer(graph, config, network, data)
         trainer.fit(verbose=True)
         valid_acc = trainer.get_valid_accuracy()
@@ -57,7 +93,7 @@ def lr_search(experiments,
         test_images, test_labels, _ = reconstruct_from_record(data.get_test_tfrecord())  # noqa
         test_images = test_images.astype(np.float32) / 255
         test_pred = trainer.predict(test_images)
-        acc_cat = accuracy_per_category(test_pred, test_labels, categories=4)
+        acc_cat = accuracy_per_category(test_pred, test_labels, categories=3)
         for i, cat_result in enumerate(acc_cat):
             name += int2command[i] + ": = {0:.6f}, ".format(cat_result)
         results.append(name)
@@ -74,6 +110,7 @@ def lr_search(experiments,
                       best_result[0],
                       best_result[2])
     file = open("learing_rate_results.txt", "w")
+    file.write(header)
     file.write("Results with different values for learing_rate\n")
     for result in results:
         result += "\n"
@@ -105,12 +142,94 @@ def main():
                         type=str,
                         default="pure",
                         help="mode for data: pure, flip, aug, bin, gray, green (default=pure)")  # noqa
-
-    parser.add_argument("-e",
+    parser.add_argument("-ex",
                         "--experiments",
                         type=int,
                         default=10,
-                        help="number of experiments")  # noqa
+                        help="number of experiments")
+    parser.add_argument('-a',
+                        '--architecture',
+                        type=int,
+                        nargs='+',
+                        help='sizes for hidden layers and output layer, should end with at least "3" !, (default=[3])',  # noqa
+                        default=[3])
+    parser.add_argument('-ac',
+                        '--activations',
+                        type=str,
+                        nargs='+',
+                        help='activations: relu, sigmoid, tanh (defaul=None)',
+                        default=None)
+    parser.add_argument("-he",
+                        "--height",
+                        type=int,
+                        default=90,
+                        help="image height (default=90)")
+    parser.add_argument("-w",
+                        "--width",
+                        type=int,
+                        default=160,
+                        help="image width (default=160)")
+    parser.add_argument('-conva',
+                        '--conv_architecture',
+                        type=int,
+                        nargs='+',
+                        help='filters for conv layers (default=[32, 64])',  # noqa
+                        default=[32, 64])
+    parser.add_argument('-k',
+                        '--kernel_sizes',
+                        type=int,
+                        nargs='+',
+                        help='kernel sizes for conv layers (default=None - 5 for every layer)',  # noqa
+                        default=None)
+    parser.add_argument('-p',
+                        '--pool_kernel',
+                        type=int,
+                        nargs='+',
+                        help='kernel sizes for pooling layers (default=None - 2 for every layer)',  # noqa
+                        default=None)
+    parser.add_argument("-b",
+                        "--batch_size",
+                        type=int,
+                        default=32,
+                        help="batch size (default=32)")
+    parser.add_argument("-e",
+                        "--epochs",
+                        type=int,
+                        default=5,
+                        help="epochs for training (default=5)")
+    parser.add_argument("-ns",
+                        "--num_steps",
+                        type=int,
+                        default=1000,
+                        help="number of steps for each epoch (default=1000)")
+    parser.add_argument("-ss",
+                        "--save_step",
+                        type=int,
+                        default=100,
+                        help="number of steps to save variables (default=100)")
+    opt_list = """optimizers: GradientDescent,
+                              Adadelta,
+                              Adagrad,
+                              Adam,
+                              Ftrl,
+                              ProximalGradientDescent,
+                              ProximalAdagrad,
+                              RMSProp"""
+    parser.add_argument("-o",
+                        "--optimizer",
+                        type=str,
+                        default="GradientDescent",
+                        help=opt_list + "(default=GradientDescent)")
+    parser.add_argument("-conv",
+                        "--conv",
+                        action="store_true",
+                        default=False,
+                        help="Use convolutional network (default=False)")
+    parser.add_argument("-di",
+                        "--divisor",
+                        type=float,
+                        default=100.0,
+                        help="value to divide the learning rate array (default=100.0)")  # noqa
     args = parser.parse_args()
     if args.mode == "bin" or args.mode == "gray" or args.mode == "green":
         channels = 1
@@ -121,9 +240,43 @@ def main():
     for record in records:
         record = args.mode + record
         new_records.append(record)
-    lr_search(experiments=args.experiments,
+
+    optimizer_dict = {"GradientDescent": tf.train.GradientDescentOptimizer, # noqa
+                      "Adadelta": tf.train.AdadeltaOptimizer,
+                      "Adagrad": tf.train.AdagradOptimizer,
+                      "Adam": tf.train.AdamOptimizer,
+                      "Ftrl": tf.train.FtrlOptimizer,
+                      "ProximalGradientDescent": tf.train.ProximalGradientDescentOptimizer, # noqa
+                      "ProximalAdagrad": tf.train.ProximalAdagradOptimizer, # noqa
+                      "RMSProp":tf.train.RMSPropOptimizer} # noqa
+
+    activations_dict = {"relu": tf.nn.relu,
+                        "sigmoid": tf.nn.sigmoid,
+                        "tanh": tf.nn.tanh}
+    if args.activations is not None:
+        activations = [activations_dict[act] for act in args.activations]
+    else:
+        activations = args.activations
+    optimizer = optimizer_dict[args.optimizer]
+
+    lr_search(mode=args.mode,
+              records=new_records,
+              height=args.height,
+              width=args.width,
               channels=channels,
-              records=new_records)
+              experiments=args.experiments,
+              architecture=args.architecture,
+              activations=activations,
+              conv_architecture=args.conv_architecture,
+              kernel_sizes=args.kernel_sizes,
+              pool_kernel=args.pool_kernel,
+              batch_size=args.batch_size,
+              epochs=args.epochs,
+              num_steps=args.num_steps,
+              save_step=args.save_step,
+              optimizer=optimizer,
+              conv=args.conv,
+              divisor=args.divisor)
 
 
 if __name__ == "__main__":
